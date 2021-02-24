a4d4694a9f46ebf6f3e9d52e760323f9b7f9475f
limi00 <75186809+limi00@users.noreply.github.com>
2021-02-24 15:16:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,23 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 替换更新adguardhome
+# rm -rf package/diy/luci-app-adguardhome && git clone https://github.com/limi00/luci-app-adguardhome.git package/diy/luci-app-adguardhome
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git project https://github.com/project-openwrt/openwrt' feeds.conf.default
+# sed -i '$a src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+# # Define My Package
+git clone https://github.com/limi00/luci-app-adguardhome.git package/diy/luci-app-adguardhome
+svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ctcgfw/luci-app-usb3disable package/diy/luci-app-usb3disable
+# git clone https://github.com/immortalwrt/openwrt-passwall.git package/diy/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app-passwall
+# git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-app-serverchan
+
+rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
